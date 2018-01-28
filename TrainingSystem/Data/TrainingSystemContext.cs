884f7766a146b1975987e07b3f99e065c89b54e8
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2018-01-28 14:47:47 -0200

    Criando estrutura inicial do projeto

---
+++ TrainingSystem/Data/TrainingSystemContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrainingSystem.Data
+{
+    public class TrainingSystemContext : DbContext
+    {
+        public TrainingSystemContext(DbContextOptions<TrainingSystemContext> options) : base(options)
+        {
+
+        }
+    }
+}
