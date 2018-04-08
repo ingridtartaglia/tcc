1589c6a296fc7777738ec63b3417eaef44a16207
Ingrid <ingrid.tartaglia@gmail.com>
2018-04-08 14:24:22 -0300

    Implementando download do certificado em pdf

--- TrainingSystem/ClientApp/src/app/platform/courses/course-certificate.component.ts
+++ TrainingSystem/ClientApp/src/app/platform/courses/course-certificate.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 import { Course } from '../../shared/models/course.model';
 import { CourseService } from '../../shared/services/course.service';
@@ -10,6 +12,7 @@ import { CourseService } from '../../shared/services/course.service';
   styleUrls: ['./course-certificate.component.css']
 })
 export class CourseCertificateComponent implements OnInit {
+  // @ViewChild('certificate') el: ElementRef;
   course: Course;
 
   constructor(private courseService: CourseService,
@@ -22,6 +25,20 @@ export class CourseCertificateComponent implements OnInit {
   getCourse() {
     const id = this.route.snapshot.params.courseId;
     this.courseService.getById(id).subscribe(course => this.course = course);
+  }
+
+  download(course: any) {
+    html2canvas(document.getElementById('certificate')).then(function (canvas) {
+      const doc = new jsPDF('l', 'mm', 'a4');
+      const img = canvas.toDataURL('image/png');
+      const width = doc.internal.pageSize.width; // 210 mm
+      const height = doc.internal.pageSize.height; // 297 mm
+      const courseName = course.name.replace(/ /g, '_');
+
+      // (image, format, xPosition, yPosition, width, height)
+      doc.addImage(img, 'JPEG', 10, 10, width, height);
+      doc.save(`Certificado_${course.appUser.firstName}_${courseName}.pdf`);
+    });
   }
 
 }
